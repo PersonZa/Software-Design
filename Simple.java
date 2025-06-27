77b132b4f5568121d0118b106a0e8e664e859d67
wittgenstein-byte <nrvsrpl@gmail.com>
2025-06-27 13:50:59 +0700

    Sdfdf
    Edit eiei

---
+++ Simple.java
@@ -0,0 +1,16 @@
+public class Simple{
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    Index("Kub",666);
+
+
+  }
+
+
+  public static void Index(String a1,int a2){
+    System.out.println("Hello Again" + a1 + " 555  " + a2);
+
+  }
+
+}
