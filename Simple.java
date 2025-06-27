b3f36195b15b77a73d6973d800b7fcf5225ca8df
wittgenstein-byte <kittikorn.sa@kkumail.com>
2025-06-27 14:36:21 +0700

    sds

---
+++ Simple.java
@@ -0,0 +1,18 @@
+
+public class Simple{
+  public static void main(String[] args) {
+
+    System.out.println("Welcome to GitHub");
+    
+    Index("PersonZa",3);
+     
+    
+  }
+
+
+  public static void Index(String a1,int a2){
+    System.out.println("Name : " + a1);
+    System.out.println("Year : " + a2);
+  }
+
+}
